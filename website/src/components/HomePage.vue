05a8d520d6cea7f0add2fa9acb6814e7a08967d5
Jonathan Simmons <jonathansimmons@awhere.com>
2020-03-17 00:01:28 -0500

    init commit

---
+++ website/src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <dev>
+    <p>Hello from Homepage</p>
+  </dev>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+};
+</script>
---
+++ website/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+import HomePage from '@/components/HomePage';
+import NotFoundPage from '@/components/NotFoundPage';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/helloworld',
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: HomePage,
+    },
+    {
+      path: '/*',
+      name: 'NotFound',
+      component: NotFoundPage
+    }
+  ],
+});
